44e01971404849d44dfca62ae3077c32479b81a8
Fabio Sdringola <124870035+FabioSM46@users.noreply.github.com>
2023-08-20 10:59:46 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export const metadata = {
+  title: "Dribbble Clone",
+  description: "Dribbble Clone built with Next.js, and Tailwind CSS",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
